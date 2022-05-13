07c4b30cfd9017ae324d81fe209083dc2a9f5fa0
A.suzuki <suzu.devworks@gmail.com>
2022-05-13 14:03:33 +0000

    Add Software Design 2020-09 Step 2-1.

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <MyCounter name="Counter 1" :initCount="5" @emitUp="getEvent" />
+    <MyCounter name="Counter 2" :initCount="10" @emitUp="getEvent" />
+    <p class="message">
+      EmitStak:
+      <ul>
+        <li v-for="item in stack" :key="item" >{{ item }}</li>
+      </ul>
+    </p>
+    <HelloWorld msg="Welcome to Your Vue.js App"/> 
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MyCounter from './components/MyCounter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    MyCounter
+  },
+  data(){
+    return {
+      stack: []
+    }
+  },
+  methods: {
+    getEvent(payload){
+      this.stack.push(payload)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.message li {
+  list-style: inside;
+}
+</style>
---
+++ src/components/MyCounter.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="counter">
+        <p>{{ name }}</p>
+        <p>Count: {{ count }}</p>
+        <button @click="increment">+1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props : {
+        name: String,
+        initCount: Number
+    },
+    data(){
+        return {
+            count: this.initCount
+        }
+    },
+    methods: {
+        increment(){
+            this.count++;
+            this.$emit("emitUp", {name: this.name, counted: this.count})
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.counter {
+    width: 200px;
+    margin: auto;
+    background-color: #ee6;
+}
+</style>
