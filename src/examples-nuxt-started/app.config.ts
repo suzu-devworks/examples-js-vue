200b22a8b68a1bb3828b99f96d19aa0db7219509
A.suzuki <suzu.devworks@gmail.com>
2023-07-25 09:14:25 +0000

    Update e-n-started learn to Configuration.

---
+++ src/examples-nuxt-started/app.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/getting-started/configuration
+
+export default defineAppConfig({
+  title: "Hello Nuxt",
+  theme: {
+    dark: true,
+    colors: {
+      primary: "#ff0000",
+    },
+  },
+})
--- src/examples-nuxt-started/nuxt.config.ts
+++ src/examples-nuxt-started/nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  // https://nuxt.com/docs/getting-started/configuration
+  runtimeConfig: {
+    // The private keys which are only available server-side
+    apiSecret: "123",
+    // Keys within public are also exposed client-side
+    public: {
+      apiBase: "/api",
+    },
+  },
   devtools: { enabled: true },
   typescript: {
     // Do not generate shims file when using Volar in VSCode (false).
